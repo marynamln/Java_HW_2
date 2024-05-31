08da0b7f8055d34c00b3640400ee9eefd16283e8
Maryna Melnyk <melnikm535@gmail.com>
2024-05-31 21:47:08 +0300

    2 commit

--- JournalModule/src/main/java/org/example/JournalRecord.java
+++ JournalModule/src/main/java/org/example/JournalRecord.java
@@ -1,12 +1,13 @@
 package org.example;
+import org.joda.time.LocalDate;
 
 public class JournalRecord {
     Subject subject;
     Student student;
-    String dateOfExam;
+    LocalDate dateOfExam;
     int mark;
 
-    public JournalRecord(Subject subject, Student student, String dateOfExam, int mark) {
+    public JournalRecord(Subject subject, Student student, LocalDate dateOfExam, int mark) {
         this.subject = subject;
         this.student = student;
         this.dateOfExam = dateOfExam;
@@ -16,13 +17,14 @@ public class JournalRecord {
     @Override
     public String toString() {
         return "Subject: " + this.subject.toString() + ", Student:  " + this.student.toString() + ", date of exam: "
-                + this.dateOfExam + ", mark: " + this.mark;
+                + this.dateOfExam.toString() + ", mark: " + this.mark;
     }
 
     public static void main(String[] args) {
         Subject subject = new Subject("Object-oriented programming", 4, 5);
         Student student = new Student("Maryna", "Melnyk");
-        System.out.println(new JournalRecord(subject, student, "30.05.2024", 95));
+        LocalDate examDate = new LocalDate(2024, 5, 30);
+        System.out.println(new JournalRecord(subject, student, examDate, 95));
     }
 
 }
