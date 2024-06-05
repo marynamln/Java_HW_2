a268ec96ec5bb6d7cf39b7f75f602925c279fb05
Maryna Melnyk <melnikm535@gmail.com>
2024-06-05 13:51:30 +0300

    4 commit

--- target/generated-sources/archetype/target/classes/archetype-resources/src/main/java/Main.java
+++ target/generated-sources/archetype/target/classes/archetype-resources/src/main/java/Main.java
@@ -1,4 +1,7 @@
-package org.example;
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
 
 public class Main {
     public static void main(String[] args) {
